57e9585491b9563d682009e6ff896e47b2de3445
yanFajR <imryanfajri@gmail.com>
2023-04-13 08:33:34 +0700

    update

--- agent.py
+++ agent.py
@@ -99,15 +99,15 @@ if __name__ == "__main__":
     observer.schedule(event_handler, path, recursive=True)
     print("Monitoring file dimulai....")
     
-    observer.start()
-    client.loop_forever()
     try:
+        client.loop_start()
+        observer.start()
         while True:
             time.sleep(1)
-
     except KeyboardInterrupt:
         observer.stop()
-        
-    observer.join()
-    time.sleep(10)
-    client.disconnect()
+        client.loop_stop()
+    
+    finally:
+        observer.join()
+        client.disconnect()
