1ca1fe9d2da079699e0534194ce12f6c059bf4de
yanFajR <imryanfajri@gmail.com>
2023-05-31 07:44:03 +0700

    agent

--- agent.py
+++ agent.py
@@ -19,22 +19,24 @@ def on_connect(client, userdata, flags, rc):
     
 def on_message(client, userdata, msg):
     command=msg.payload.decode()
-    print(command)
     if msg.topic == "ScanRequest":
         try:
-            print("=== scanning to server ===")
             dict_entry = json.loads(command)
             if os.path.exists(dict_entry['file_path']) and ip_address == dict_entry['client_ip']:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                server_address = ('103.59.95.89', 5000)
-                sock.connect(server_address)
-                print("connected to server")
-                with open(dict_entry['file_path'], 'rb') as f:
-                    file_contents = f.read()
-                print("file readed")
-                serialized_contents = pickle.dumps(file_contents)
-                sock.sendall(serialized_contents)
-                sock.close()
+                try:
+                    print("=== scanning to server ===")
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    server_address = ('103.59.95.89', 5000)
+                    sock.connect(server_address)
+                    print("connected to server")
+                    with open(dict_entry['file_path'], 'rb') as f:
+                        file_contents = f.read()
+                    print("file readed")
+                    serialized_contents = pickle.dumps(file_contents)
+                    sock.sendall(serialized_contents)
+                    sock.close()
+                except Exception as e:
+                    print("Error", e)
         except Exception as e:
             print("Error", e)
     elif msg.topic == "ScanResult":
@@ -48,18 +50,11 @@ def on_message(client, userdata, msg):
 
 class FilleDetector(FileSystemEventHandler):
     def on_created(self, event):
-        if (event.is_directory == False):
+        if (not event.is_directory and not event.src_path.endswith(".part")):
             print(event.src_path, "New file Detected")
             full_path = os.path.abspath(event.src_path)
-            md5_returned = hashlib.md5()
-            with open(event.src_path, 'rb') as f:
-                while True:
-                    data = f.read(65536)
-                    if not data:
-                        break
-                    md5_returned.update(data)
-            
-            md5_returned = md5_returned.hexdigest()
+            time.sleep(1)
+            md5_returned = calculate_hash(full_path)
 
             result = { 
                 'client_hash': md5_returned,
@@ -74,16 +69,28 @@ class FilleDetector(FileSystemEventHandler):
     
     def on_deleted(self, event):
         if (event.is_directory == False):
-            print("=================================")
-            print(event.src_path, event.event_type)
             full_path = os.path.abspath(event.src_path)
-            result = { 
-                'client_ip': ip_address,
-                'file_path': full_path,
-                }
-            string_payload = json.dumps(result)
-            client.publish("DeletedFile", string_payload)
-            print(string_payload)
+            
+            if not os.path.exists(full_path):
+                print("=================================")
+                print(event.src_path, event.event_type)
+                
+                result = { 
+                    'client_ip': ip_address,
+                    'file_path': full_path,
+                    }
+                string_payload = json.dumps(result)
+                client.publish("DeletedFile", string_payload)
+                print(string_payload)
+            
+def calculate_hash(file_path):
+    hash_algorithm = hashlib.md5()
+    with open(file_path, 'rb') as file:
+        for chunk in iter(lambda: file.read(65536), b""):
+            hash_algorithm.update(chunk)
+
+    md5_returned = hash_algorithm.hexdigest()
+    return md5_returned
 
 if __name__ == "__main__":
     ip_address = socket.gethostbyname(socket.gethostname())
